95dfaede6258d1dce5797d3454f9b1f669b2ce62
Joel16 <sauskethunderkid@gmail.com>
2018-05-17 11:26:57 -0500

    Fix SwitchIdent_GetHardwareType()
    Version 4.00+ if returns 0x3, it should be Mariko.

--- source/kernel.c
+++ source/kernel.c
@@ -11,16 +11,16 @@ const char *SwitchIdent_GetVersion(void)
 
 	const char *versions[] =
 	{
-		"Unknown0",  // 0x0
-		"Unknown1",  // 0x1
+		"Unknown0",
+		"Unknown1",
 		"1.0.0",     // 0x2
 		"2.0.0",     // 0x3
 		"3.0.0",     // 0x4
 		"3.0.2",     // 0x5
 		"4.0.0",     // 0x6
 		"5.0.0",     // 0x7
-		"Unknown2"   // 0x8
-		"Unknown3"   // Couldn't get version
+		"Unknown2",
+		"Unknown3"
 	};
 
 	if (R_FAILED(ret = splGetConfig(SplConfigItem_Version, &version)))
@@ -32,19 +32,38 @@ const char *SwitchIdent_GetVersion(void)
 const char *SwitchIdent_GetHardwareType(void)
 {
 	Result ret = 0;
-	u64 hardware_type = 2;
+	u64 hardware_type = 4, version = 0;
 
-	const char *hardware[] =
+	if (R_FAILED(ret = splGetConfig(SplConfigItem_Version, &version)))
+		printf("splGetConfig(SplConfigItem_Version) failed: 0x%x.\n\n", ret);
+
+	version = version + 1;
+
+	const char *hardware_300[] =
 	{
 		"Icosa",
 		"Copper",
+		"Unknown",
+		"Unknown",
+		"Unknown"
+	};
+
+	const char *hardware_400[] =
+	{
+		"Icosa",
+		"Copper",
+		"Unknown",
+		"Mariko",
 		"Unknown"
 	};
 
 	if (R_FAILED(ret = splGetConfig(SplConfigItem_HardwareType, &hardware_type)))
 		printf("splGetConfig(SplConfigItem_HardwareType) failed: 0x%x.\n\n", ret);
 
-	return hardware[hardware_type];
+	if (version < 0x6)
+		return hardware_300[hardware_type];
+	
+	return hardware_400[hardware_type];
 }
 
 const char *SwitchIdent_GetUnit(void)
