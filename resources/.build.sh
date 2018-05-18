d5f09959bcc182e7f30d87dd42c43cd798be13e4
Joel16 <sauskethunderkid@gmail.com>
2018-05-18 14:27:40 -0500

    Remove icon.jpg after building

--- resources/.build.sh
+++ resources/.build.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 
 make clean && make
-rm .gitattributes .travis.yml LICENSE Makefile README.md
-rm -rf build include resources source SwitchIdent.elf SwitchIdent.nacp SwitchIdent.nso SwitchIdent.pfs0
+rm .gitattributes .travis.yml icon.jpg LICENSE Makefile README.md SwitchIdent.elf SwitchIdent.nacp SwitchIdent.nso SwitchIdent.pfs0
+rm -rf build include resources source
