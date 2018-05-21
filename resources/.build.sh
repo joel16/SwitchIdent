35f51597246c798ce6c6e6956747c4e290c29f3c
Joel16 <sauskethunderkid@gmail.com>
2018-05-21 10:17:10 -0500

    Fix travis builds (again)

--- resources/.build.sh
+++ resources/.build.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 
 make clean && make
-rm .gitattributes .travis.yml icon.jpg LICENSE Makefile README.md SwitchIdent.elf SwitchIdent.nacp SwitchIdent.nso SwitchIdent.pfs0
+rm .gitattributes .travis.yml devkitpro-pacman.deb icon.jpg LICENSE Makefile README.md SwitchIdent.elf SwitchIdent.nacp SwitchIdent.nso SwitchIdent.pfs0 
 rm -rf build include resources source
--- resources/.travis-deps.sh
+++ resources/.travis-deps.sh
@@ -2,20 +2,11 @@
 set -e
 set -x
 
-# Build and install devkitARM + ctrulib
-mkdir -p $DEVKITPRO
-cd $DEVKITPRO
-wget -N https://raw.githubusercontent.com/devkitPro/installer/master/perl/devkitA64update.pl
-chmod +x ./devkitA64update.pl
-./devkitA64update.pl
-
 # Get latest libnx and overwrite bundled one
 mkdir libnx-update && cd libnx-update
 git clone https://github.com/switchbrew/libnx.git
 cd libnx/
-make && make install
+make
+sudo make install
 cd ../../
 rm -rf libnx-update/
-
-rm *.bz2
-rm devkitA64update.pl
