8cd13cb647c54bbd6954f6f16c34d251fa609d44
Joel16 <sauskethunderkid@gmail.com>
2018-06-11 13:45:12 -0500

    Fix travis builds
    TODO: Switch to docker.

--- resources/.build.sh
+++ resources/.build.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 make clean && make
-mv gui/gui.nro ../
-mv console/console.nro ../
+mv gui/gui.nro .
+mv console/console.nro .
 rm .gitattributes .travis.yml devkitpro-pacman.deb LICENSE Makefile README.md
 rm -rf common console gui resources source
